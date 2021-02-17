25cc30573d6dc217df19daf5f827db67a3d01b8e
drantini <52494986+antiiii@users.noreply.github.com>
2021-02-17 11:02:59 +0100

    Add project files.

---
+++ ImageToText/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using IronOcr;
+using Microsoft.Win32;
+using System.IO;
+using System.Drawing;
+
+namespace ImageToText
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private Bitmap inputBitmap;
+        private bool was_converted = false;
+        private bool was_uploaded = false;
+        private bool denoise = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            if (fd.ShowDialog() == true)
+            {
+                inputImage.Source = new BitmapImage(new Uri(fd.FileName));
+                inputBitmap = BitmapImage2Bitmap(new BitmapImage(new Uri(fd.FileName)));
+                was_uploaded = true;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (was_uploaded)
+            {
+                using (var input = new OcrInput())
+                {
+                    input.AddImage(inputBitmap);
+                    input.Deskew();
+                    if (DeNoise.IsChecked == true)
+                    {
+                        input.DeNoise();
+                    }
+
+                    var Result = new IronTesseract().Read(input).Text;
+                    OutputText.Text = Result;
+                    was_uploaded = false;
+                    was_converted = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Upload image first.");
+            }
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (was_converted)
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                if (fd.ShowDialog() == true)
+                {
+                    File.WriteAllText(fd.FileName, OutputText.Text);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please convert file first!");
+            }
+        }
+
+    }
+}
